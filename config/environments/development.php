becd6b8e723a0b595f0bb9ed1244a145afa19b68
George Mamadashvili <georgemamadashvili@gmail.com>
2020-05-16 13:04:53 +0400

    Hello world

---
+++ config/environments/development.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Development Environment
+ *
+ * @package Dekode
+ */
+
+declare( strict_types = 1 );
+
+define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
+define( 'SCRIPT_DEBUG', true );
